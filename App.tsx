450c2f3bf97f7604dd6f8e675ca252773f4affba
Matan Shaviro <matan3sh@gmail.com>
2023-04-10 18:06:01 +0300

    Navigation Setup

--- App.tsx
+++ App.tsx
@@ -1,19 +1,25 @@
 import React from 'react';
-import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import 'react-native-gesture-handler';
+
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+
 import Home from './src/screens/Home/Home';
+import AttractionDetails from './src/screens/AttractionDetails/AttractionDetails';
+import Gallery from './src/screens/Gallery/Gallery';
+
+const Stack = createStackNavigator();
 
 function App(): JSX.Element {
   return (
-    <SafeAreaView style={styles.safeView}>
-      <Home />
-    </SafeAreaView>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="AttractionDetails" component={AttractionDetails} />
+        <Stack.Screen name="Gallery" component={Gallery} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  safeView: {
-    flex: 1,
-  },
-});
 
 export default App;
---
+++ src/screens/Gallery/Gallery.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+function Gallery() {
+  return (
+    <View>
+      <Text>Gallery</Text>
+    </View>
+  );
+}
+
+export default React.memo(Gallery);
